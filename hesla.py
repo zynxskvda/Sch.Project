a8d9c364f8ef8f5d15a60be5d45bc4d68e00073c
JonaslazarVDA <106740298+JonaslazarVDA@users.noreply.github.com>
2022-06-09 11:43:12 +0200

    :)
    hesla :)

---
+++ hesla.py
@@ -0,0 +1,16 @@
+import random, string
+numb = a
+chars = ''
+if input("include alphabet (y): "):
+	chars += 'abcdefghijklmopqrstuvwxyz'
+if input("include capitals (y): "):
+	chars += chars.upper()
+if input("include numbers (y): "):
+	chars += '0123456789'
+if input("include special characters (y): "):
+	chars += '+-*/-\\\"\'!@#$%^&(){}[]'
+while !numb.isnumeric():
+	numb = input("zadej číslo: ")
+for _ in range(10):
+	print(''.join(random.choice(chars) for _ in range(numb)))
+readKey()
